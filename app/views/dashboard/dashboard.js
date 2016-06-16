9c9096b3a81fa573d60f9fc74095b375a97dd4d3
Davis Batten <davis.batten@zirous.com>
2016-06-16 13:17:23 -0500

    installed ngTagsInput

--- app/views/dashboard/dashboard.js
+++ app/views/dashboard/dashboard.js
@@ -2,17 +2,48 @@
 
 angular.module('cliffhanger.dashboard', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/dashboard', {
-    templateUrl: 'views/dashboard/dashboard.html',
-    controller: 'DashboardCtrl'
-  });
+.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/dashboard', {
+        templateUrl: 'views/dashboard/dashboard.html',
+        controller: 'DashboardCtrl'
+    });
 }])
 
-.controller('DashboardCtrl', function($rootScope, $log, $scope) {
+.controller('DashboardCtrl', function ($rootScope, $log, $scope, $q, $filter) {
 
-    $scope.testGlobal = function() {
+    $scope.testGlobal = function () {
         $log.log($rootScope);
     };
+
+    $scope.tags = [
+        {
+            name: "SSN",
+            description: "social security",
+        },
+        {
+            name: "ZIP",
+            description: "zip code"
+        },
+        {
+            name: "address1",
+            description: "first line of addresss"
+        }
+    ]
+
+    $scope.loadTags = function (query) {
+        var deferred = $q.defer();
+        var filteredTags = $filter('filter')($scope.tags, {
+            name: query
+        });
+
+        if (filteredTags.length == 0) $scope.noResults = true;
+        else $scope.noResults = false;
+
+        deferred.resolve(filteredTags);
+
+        return deferred.promise;
+    };
+
+
 
 });
