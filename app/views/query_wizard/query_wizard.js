3db1916b0f4b1c279d608cc72cf06bc39f27493f
Davis Batten <davis.batten@zirous.com>
2016-07-01 16:42:23 -0500

    better comments/documentation

--- app/views/query_wizard/query_wizard.js
+++ app/views/query_wizard/query_wizard.js
@@ -88,14 +88,19 @@ queries.controller('QueryWizardCtrl', function ($scope, $uibModalInstance, $log,
 
         for (var i = 0; i < dataset.attributes.length; i++) {
             var a = dataset.attributes[i];
+            //select all
             if ($scope.selected[dataset.name]) {
+                //add column to selection
                 if (a.tag.name != $scope.selectedTags[0].name) {
                     a.db_table_name = dataset.db_table_name;
                     a.selected = true;
                     $scope.selectedColumns.push(a);
                 }
-            } else {
+            }
+            //deselect all
+            else {
                 a.selected = false;
+                //remove column from selection
                 for (var j = 0; j < $scope.selectedColumns.length; j++) {
                     if ($scope.selectedColumns[j].name == a.name) {
                         $scope.selectedColumns.splice(j, 1);
