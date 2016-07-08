6b8c8e2c27aaafef5fa9e6f0716067788df1dfa5
mkrueger <matt.krueger@zirous.com>
2016-07-08 11:12:36 -0500

    added ui-grid, placeholders for ui-grid related methods

--- app/app.js
+++ app/app.js
@@ -6,6 +6,7 @@ app = angular.module('cliffhanger', [
     , 'ngTagsInput'
     , 'ngSanitize'
     , 'ngCsv'
+    , 'ui.grid'
     , 'cliffhanger.version',
 
     //My modules
@@ -26,8 +27,8 @@ config(['$locationProvider', '$routeProvider', function ($locationProvider, $rou
 }]).run(function ($rootScope) {
     //set base Url for the REST API
 
-    //    $rootScope.baseUrl = 'http://localhost:8080/cliffhanger'; //development
-    $rootScope.baseUrl = 'http://hangingonbyanicepick.eastus2.cloudapp.azure.com:8080/cliffhanger-0.1'; //production
+    $rootScope.baseUrl = 'http://localhost:8080/cliffhanger'; //development
+    //$rootScope.baseUrl = 'http://hangingonbyanicepick.eastus2.cloudapp.azure.com:8080/cliffhanger-0.1'; //production
 
 }).directive('prevent-default', function ($rootScope) {
 
