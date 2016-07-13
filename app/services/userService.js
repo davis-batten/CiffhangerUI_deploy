78f2ecc12167f389ee64ec8d190133cffd458866
Molly Hayes <molly.hayes@zirous.com>
2016-07-13 17:19:37 -0500

    attempt at implementing getUser

--- app/services/userService.js
+++ app/services/userService.js
@@ -1,104 +1,94 @@
 //service for dealing with Meta-Types on the Grails backend using REST API
-angular.module('cliffhanger.superuser')
-    .service('userService', function ($log, $http, $rootScope, $q) {
-
+angular.module('cliffhanger.superuser').service('userService', function ($log, $http, $rootScope, $q) {
+    $rootScope.user = {}
+    this.login = function (username, password) {
+        var input = {
+            username: username
+            , password: password
+        }
+        return $http.post($rootScope.baseUrl + '/user/login', input).then(
+            //success callback
+            function (response) {
+                $log.info('Successful login!', response);
+                //update $rootScope with user profile details
+                $rootScope.user = response.data.data;
+                return response.data;
+            }, //error callback
+            function (response) {
+                $log.warn('Failure!');
+                $log.warn(response);
+                return $q.reject(response.data);
+            });
+    }
+    this.logout = function () {
         $rootScope.user = {}
-
-        this.login = function (username, password) {
-            var input = {
-                username: username,
-                password: password
-            }
-
-            return $http.post($rootScope.baseUrl + '/user/login', input)
-                .then(
-                    //success callback
-                    function (response) {
-                        $log.info('Successful login!', response);
-                        //update $rootScope with user profile details
-                        $rootScope.user = response.data.data;
-                        return response.data;
-                    },
-                    //error callback
-                    function (response) {
-                        $log.warn('Failure!');
-                        $log.warn(response);
-                        return $q.reject(response.data);
-
-                    }
-                );
+    }
+    this.register = function (newUser) {
+        return $http.post($rootScope.baseUrl + '/user/create', newUser).then(
+            //success callback
+            function (response) {
+                $log.info('Success!', response);
+                return response.data;
+            }, //error callback
+            function (response) {
+                $log.warn('Failure!');
+                $log.warn(response);
+                return $q.reject(response.data);
+            });
+    }
+    this.getAllUsers = function () {
+        return $http.get($rootScope.baseUrl + '/user/getAllUsers').then(
+            //success callback
+            function (response) {
+                $log.info('Success', response);
+                return response.data;
+            }, //Error callback
+            function (response) {
+                $log.warn('Failure');
+                $log.warn(response);
+                return $q.reject(response.data);
+            });
+    }
+    this.updateUser = function (username, newUser) {
+            return $http.put($rootScope.baseUrl + '/user/update/' + username, newUser).then(
+                //Success callback
+                function (response) {
+                    $log.info('Success', response);
+                    return response.data;
+                }, //Error callback
+                function (response) {
+                    $log.warn('Failure');
+                    $log.warn(response);
+                    return $q.reject(response.data);
+                });
         }
-
-        this.logout = function () {
-            $rootScope.user = {}
-        }
-
-        this.register = function (newUser) {
-            return $http.post($rootScope.baseUrl + '/user/create', newUser)
-                .then(
-                    //success callback
-                    function (response) {
-                        $log.info('Success!', response);
-                        return response.data;
-                    },
-                    //error callback
-                    function (response) {
-                        $log.warn('Failure!');
-                        $log.warn(response);
-                        return $q.reject(response.data);
-
-                    }
-                );
-        }
-
-        this.getAllUsers = function () {
-            return $http.get($rootScope.baseUrl + '/user/getAllUsers')
-                .then(
-                    //success callback
-                    function (response) {
-                        $log.info('Success', response);
-                        return response.data;
-                    },
-                    //Error callback
-                    function (response) {
-                        $log.warn('Failure');
-                        $log.warn(response);
-                        return $q.reject(response.data);
-                    }
-                );
-        }
-
-        this.updateUser = function (username, newUser) {
-            return $http.put($rootScope.baseUrl + '/user/update/' + username, newUser)
-                .then(
-                    //Success callback
-                    function (response) {
-                        $log.info('Success', response);
-                        return response.data;
-                    },
-                    //Error callback
-                    function (response) {
-                        $log.warn('Failure');
-                        $log.warn(response);
-                        return $q.reject(response.data);
-                    }
-                );
-        }
-
-        this.deleteUser = function (username) {
-            return $http.delete($rootScope.baseUrl + '/user/delete/' + username)
-                .then(
-                    //Success callback
-                    function (response) {
-                        $log.info('Success', response);
-                        return response.data;
-                    },
-                    //Error callback
-                    function (response) {
-                        $log.warn('Failure');
-                        $log.warn(response);
-                        return $q.reject(response.data);
-                    }
-                );
-        }
-    });
+        //this service method gets an existing user from the backend
+    this.getUser = function (username) {
+        $log.log(JSON.stringify(username));
+        return $http.get($rootScope.baseUrl + '/user/get' + username).then(
+            //success callback
+            function (response) {
+                $log.info('Success!');
+                $log.info(response);
+                return reponse.data;
+            }, //error callback
+            function (response) {
+                $log.warn('Failure!');
+                $log.warn(response);
+                return $q.reject(response.data);
+            });
+    }
+    this.deleteUser = function (username) {
+        return $http.delete($rootScope.baseUrl + '/user/delete/' + username).then(
+            //Success callback
+            function (response) {
+                $log.info('Success', response);
+                return response.data;
+            }, //Error callback
+            function (response) {
+                $log.warn('Failure');
+                $log.warn(response);
+                return $q.reject(response.data);
+            });
+    }
+});
