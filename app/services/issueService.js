be2d1faf18c871bbac14dc8aa30d9e94ef00fa5a
Molly Hayes <molly.hayes@zirous.com>
2016-07-18 16:40:38 -0500

    sortby and filter both work for messageboard

--- app/services/issueService.js
+++ app/services/issueService.js
@@ -1,108 +1,94 @@
 angular.module('cliffhanger.issue').service('issueService', function ($log, $http, $rootScope, $q) {
-
     var baseUrl = $rootScope.baseUrl;
-
     //create a new issue thread
     this.createIssue = function (newIssue) {
-        //TODO
-        return $http.post(baseUrl + '/discussionThread/create', newIssue).then(
-            //success callback
-            function (response) {
-                $log.debug(response);
-                return response.data;
-            },
-            //error callback
-            function (response) {
-                $log.error(response);
-                return $q.reject(response.data);
-            });
-    }
-
-    //get a list of all issues
-    this.getAllIssues = function () {
-        //TODO
-        return $http.get(baseUrl + '/discussionThread/list').then(
-            //success callback
-            function (response) {
-                $log.debug(response);
-                return response.data;
-            },
-            //error callback
-            function (response) {
-                $log.error(response);
-                return $q.reject(response.data);
-            });
-    }
-
-    //get a single issue
-    this.getIssue = function (threadId) {
-        return $http.get(baseUrl + '/discussionThread/get/' + threadId).then(
-            //success callback
-            function (response) {
-                $log.debug(response);
-                return response.data;
-            },
-            //error callback
-            function (response) {
-                $log.error(response);
-                return $q.reject(response.data);
-            });
-    }
-
-    //toggle open/close an issue
-    this.toggleOpen = function (threadId, closerUsername) {
-        //TODO
-        var closer = {
-            closer: closerUsername
+            //TODO
+            return $http.post(baseUrl + '/discussionThread/create', newIssue).then(
+                //success callback
+                function (response) {
+                    $log.debug(response);
+                    return response.data;
+                }
+                , //error callback
+                function (response) {
+                    $log.error(response);
+                    return $q.reject(response.data);
+                });
         }
-
-        $log.info('closer', closer);
-        return $http.put(baseUrl + '/discussionThread/toggleOpen/' + threadId, closer).then(
-            //success callback
-            function (response) {
-                $log.debug(response);
-                return response.data;
-            },
-            //error callback
-            function (response) {
-                $log.error(response);
-                return $q.reject(response.data);
-            });
-    }
-
-
-    //post a new comment
+        //get a list of all issues
+    this.getAllIssues = function () {
+            //TODO
+            return $http.get(baseUrl + '/discussionThread/list').then(
+                //success callback
+                function (response) {
+                    $log.debug(response);
+                    return response.data;
+                }
+                , //error callback
+                function (response) {
+                    $log.error(response);
+                    return $q.reject(response.data);
+                });
+        }
+        //get a single issue
+    this.getIssue = function (threadId) {
+            return $http.get(baseUrl + '/discussionThread/get/' + threadId).then(
+                //success callback
+                function (response) {
+                    $log.debug(response);
+                    return response.data;
+                }
+                , //error callback
+                function (response) {
+                    $log.error(response);
+                    return $q.reject(response.data);
+                });
+        }
+        //toggle open/close an issue
+    this.toggleOpen = function (threadId, closerUsername) {
+            //TODO
+            var closer = {
+                closer: closerUsername
+            }
+            $log.info('closer', closer);
+            return $http.put(baseUrl + '/discussionThread/toggleOpen/' + threadId, closer).then(
+                //success callback
+                function (response) {
+                    $log.debug(response);
+                    return response.data;
+                }
+                , //error callback
+                function (response) {
+                    $log.error(response);
+                    return $q.reject(response.data);
+                });
+        }
+        //post a new comment
     this.postComment = function (newComment) {
-
-        return $http.post(baseUrl + '/comment/post', newComment).then(
-            //success callback
-            function (response) {
-                $log.debug(response);
-                return response.data;
-            },
-            //error callback
-            function (response) {
-                $log.error(response);
-                return $q.reject(response.data);
-            });
-    }
-
-    //fetch all comments for an issue
+            return $http.post(baseUrl + '/comment/post', newComment).then(
+                //success callback
+                function (response) {
+                    $log.debug(response);
+                    return response.data;
+                }
+                , //error callback
+                function (response) {
+                    $log.error(response);
+                    return $q.reject(response.data);
+                });
+        }
+        //fetch all comments for an issue
     this.getComments = function (threadId) {
-
-        return $http.get(baseUrl + '/comment/getAllByThreadId/' + threadId).then(
+        return $http.get(baseUrl + '/comment/getAllByThread/' + threadId).then(
             //success callback
             function (response) {
                 $log.debug(response);
                 return response.data;
-            },
-            //error callback
+            }
+            , //error callback
             function (response) {
                 $log.error(response);
                 return $q.reject(response.data);
             });
     }
-
-
-
 });
