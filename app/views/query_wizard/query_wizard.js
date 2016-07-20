45b0f2c4ad64297f14d9841d469b456e1d18a406
Molly Hayes <molly.hayes@zirous.com>
2016-07-20 16:50:12 -0500

    I fixed the thing that bugged me a lot, but just with height not dynamically

--- app/views/query_wizard/query_wizard.js
+++ app/views/query_wizard/query_wizard.js
@@ -1,7 +1,6 @@
 'use strict';
 var query_wizard = angular.module('cliffhanger.query_wizard', ['ngRoute', 'ngSanitize', 'ngCsv']);
 query_wizard.controller('QueryWizardCtrl', function ($scope, $rootScope, $uibModalInstance, $log, datasets, queryService, issueService) {
-    
     $scope.query = {}; //container for query
     $scope.alerts = [];
     $scope.dataTypeCheck = [];
@@ -29,11 +28,10 @@ query_wizard.controller('QueryWizardCtrl', function ($scope, $rootScope, $uibMod
     $scope.postReportSubmissionMessage;
     $scope.reportSubmitted = false;
     $scope.newProblemInput = {
-        subject: '',
-        body: ''    
+        subject: ''
+        , body: ''
     }
     $scope.shouldShowNotifyDevsForm = false;
-
     //method responsible for handling changes due to checkboxes
     //d -> item selected/deselected
     //selections -> array to add/remove item
@@ -41,7 +39,8 @@ query_wizard.controller('QueryWizardCtrl', function ($scope, $rootScope, $uibMod
         if (d.selected) {
             if (selections.indexOf(d) > -1) selections.push(angular.copy(d));
             else selections.push(d);
-        } else {
+        }
+        else {
             for (var i = 0; i < selections.length; i++) {
                 $log.debug('remove?', selections[i]);
                 if (selections[i].name == d.name) {
@@ -58,7 +57,8 @@ query_wizard.controller('QueryWizardCtrl', function ($scope, $rootScope, $uibMod
         if (column.selected) {
             column.db_table_name = dataset.db_table_name;
             selections.push(column);
-        } else {
+        }
+        else {
             for (var i = 0; i < selections.length; i++) {
                 $log.debug(selections[i]);
                 if (selections[i].name == column.name) {
@@ -85,7 +85,8 @@ query_wizard.controller('QueryWizardCtrl', function ($scope, $rootScope, $uibMod
                 }
             }
             $log.debug($scope.tags);
-        } else $scope.tags = [];
+        }
+        else $scope.tags = [];
     };
     //advance the modal to the next step
     $scope.next = function () {
@@ -93,15 +94,17 @@ query_wizard.controller('QueryWizardCtrl', function ($scope, $rootScope, $uibMod
         if ($scope.step == 2) {
             $scope.numJoins++;
             $log.debug($scope.numJoins);
-        } else if ($scope.step == 3) {
+        }
+        else if ($scope.step == 3) {
             $scope.archiveTags();
-        } else if ($scope.step == 4) {
+        }
+        else if ($scope.step == 4) {
             $scope.buildQuery();
-        } else if ($scope.step == 5) {
+        }
+        else if ($scope.step == 5) {
             $scope.runQuery();
         }
     };
-
     //go back a step in the modal
     $scope.previous = function () {
         $scope.step--;
@@ -122,7 +125,6 @@ query_wizard.controller('QueryWizardCtrl', function ($scope, $rootScope, $uibMod
             $scope.tableResult = null;
         }
     };
-
     $scope.archiveDatasets = function () {
         for (var i = 0; i < $scope.selectedDatasets.length; i++) {
             if ($scope.alreadyUsedDatasets.indexOf($scope.selectedDatasets[i]) == -1) {
@@ -181,17 +183,15 @@ query_wizard.controller('QueryWizardCtrl', function ($scope, $rootScope, $uibMod
         }
         $log.debug($scope.statement);
     };
-
     //build a new query given the user's choices
     $scope.buildQuery = function () {
         //query input packaged
         var queryInput = {
-            datasets: $scope.selectedDatasets,
-            joinTag: $scope.alreadyUsedTags,
-            addJoinColumn: true,
-            columns: $scope.selectedColumns
+            datasets: $scope.selectedDatasets
+            , joinTag: $scope.alreadyUsedTags
+            , addJoinColumn: true
+            , columns: $scope.selectedColumns
         }
-
         queryService.buildQuery(queryInput).then(function (response) {
             //success callback
             if (response.status == 'Success') {
@@ -200,24 +200,24 @@ query_wizard.controller('QueryWizardCtrl', function ($scope, $rootScope, $uibMod
                 $log.debug('response', response);
                 if ($scope.query[1] == true) {
                     $scope.dataTypeCheck.push({
-                        msg: "Join Attributes data type mismatch.",
-                        type: 'warning'
+                        msg: "Join Attributes data type mismatch."
+                        , type: 'warning'
                     });
                 }
-            } else { //failure callback
+            }
+            else { //failure callback
                 $scope.progressType = 'danger';
                 $scope.buildQueryError = true;
                 $log.error(response.data);
             }
         });
-
     };
-
     //complete the modal
     $scope.save = function () {
         if ($scope.statement != undefined) {
             $scope.newQuery.sqlString = $scope.query[0] + $scope.statement.text;
-        } else {
+        }
+        else {
             $scope.newQuery.sqlString = $scope.query[0];
         }
         if ($scope.newQuery.description == null || $scope.newQuery.description == undefined) {
@@ -226,27 +226,28 @@ query_wizard.controller('QueryWizardCtrl', function ($scope, $rootScope, $uibMod
         queryService.saveQuery($scope.newQuery).then(function (data) {
             if (data.status == 'Success') {
                 $scope.alerts.push({
-                    msg: "Query Successfully Saved!",
-                    type: 'success'
+                    msg: "Query Successfully Saved!"
+                    , type: 'success'
                 });
                 $scope.showExit = true;
                 $log.debug(data);
-            } else {
+            }
+            else {
                 $scope.alerts.push({
-                    msg: "Save Failed",
-                    type: 'danger'
+                    msg: "Save Failed"
+                    , type: 'danger'
                 });
                 $log.debug(data);
             }
             $log.debug($scope.statement);
         });
     };
-
     $scope.runQuery = function () {
         $scope.loadingPreview = true;
         if ($scope.statement != undefined) {
             var query = $scope.query[0] + $scope.statement.text;
-        } else {
+        }
+        else {
             var query = $scope.query[0];
         }
         queryService.runQuery(query).then(function (response) { //success callback
@@ -256,24 +257,22 @@ query_wizard.controller('QueryWizardCtrl', function ($scope, $rootScope, $uibMod
                     $scope.progressType = 'danger';
                     $scope.queryRanFine = false;
                     $scope.noResults = true;
-                    $scope.newProblemInput.body = "Cliffhanger Report: Running the join query succeeded but the result table was empty. \nQuery used: " + query;
-                } else {
+                    $scope.newProblemInput.body = "Cliffhanger Report: Running the join query succeeded but the result table was empty. \nQuery used: \n" + query;
+                }
+                else {
                     $scope.tableResult = response;
                     $scope.progressType = 'success';
                 }
-
             }, //failure to connect
             function (data) {
                 $scope.loadingPreview = false;
                 $scope.progressType = 'danger';
                 $scope.queryRanFine = false;
                 $scope.connectionFailed = true;
-                $scope.newProblemInput.body = "Cliffhanger Report: HTTP call during method runQuery() in QueryService.js was not status 200. There is likely a problem with the REST service or Hive. \nQuery used: " + query;
-
+                $scope.newProblemInput.body = "Cliffhanger Report: HTTP call during method runQuery() in QueryService.js was not status 200. There is likely a problem with the REST service or Hive. \nQuery used: \n" + query;
                 $log.error('Failed to connect to server');
             });
     };
-
     $scope.$watch('selectedDatasets', function () {
         if ($scope.step == 2) {
             $log.debug("load tags");
@@ -292,15 +291,12 @@ query_wizard.controller('QueryWizardCtrl', function ($scope, $rootScope, $uibMod
         $scope.numJoins++;
         $log.debug('dataset', $scope.selectedDatasets);
     };
-
     $scope.showNotifyDevsForm = function () {
         $scope.shouldShowNotifyDevsForm = true;
     };
-
     $scope.hideNotifyDevsForm = function () {
         $scope.shouldShowNotifyDevsForm = false;
     };
-
     $scope.reportProblem = function () {
         $uibModalInstance.dismiss('cancel');
         issueService.createIssue($scope.newProblemInput).then(function (response) {
@@ -313,5 +309,4 @@ query_wizard.controller('QueryWizardCtrl', function ($scope, $rootScope, $uibMod
             $scope.reportSubmitted = true;
         });
     };
-
 });
