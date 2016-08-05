efbd2c3b1d5ba96cfcab51fe8873dace57f1f7a9
Chris Kelley <ckelley@zirous.com>
2016-08-05 12:05:38 -0500

    comments and spacing

--- app/views/queries/queries.js
+++ app/views/queries/queries.js
@@ -8,36 +8,49 @@ angular.module('cliffhanger.queries', ['ngRoute', 'ngSanitize', 'ngCsv']).config
 var queries = angular.module('cliffhanger.queries');
 //main controller for queries page
 queries.controller('QueriesCtrl', function ($scope, $uibModal, $log, $location, queryService, issueService, $rootScope) {
+    //list of selected queries
     $scope.selected = [];
-    $scope.showNoQueriesMessage = false;
-    $scope.download = false;
+    //list of alerts to show to user
+    $scope.alerts = [];
     //queries sorted by date created by default
     $scope.propertyName = 'dateCreated';
-    $scope.alerts = []; //list of alerts to show to user
+    //set theme color
+    $rootScope.theme.color = 'blue';
+    //boolean for sort by drop down
+    $scope.isCollapsed = true;
+    //boolean for if there are queries list from server
+    $scope.showNoQueriesMessage = false;
+    //boolean for if query results have been downloaded
+    $scope.download = false;
+
+
     //closes an alert
     $scope.closeAlert = function (index) {
         $scope.alerts.splice(index, 1);
     };
-    //set theme color
-    $rootScope.theme.color = 'blue';
+
+    //redirect to login if invalid user
     $rootScope.$watch('user', function () {
         if ($rootScope.user.username == null) {
             $location.url('/');
         }
     });
-    $scope.isCollapsed = true;
+
     //for logout dropdown
     $scope.toggleLogoutDropdown = function ($event) {
         $event.preventDefault();
         $event.stopPropagation();
         $scope.status.logoutisopen = !$scope.status.logoutisopen;
     };
+
     //for sort by dropdown
     $scope.toggleSortByDropdown = function ($event) {
         $event.preventDefault();
         $event.stopPropagation();
         $scope.status.sortbyisopen = !$scope.status.sortbyisopen;
     };
+
+    //sets the sort by parameters
     $scope.setSort = function (sort) {
         $scope.propertyName = sort;
         $scope.reverse = false;
@@ -46,6 +59,8 @@ queries.controller('QueriesCtrl', function ($scope, $uibModal, $log, $location, 
             $scope.reverse = true;
         }
     };
+
+    //get a list of all the queries
     $scope.getQueries = function () {
         if ($rootScope.user.roles == 'ROLE_ADMIN') {
             $scope.getAllQueries = function () {
@@ -76,6 +91,7 @@ queries.controller('QueriesCtrl', function ($scope, $uibModal, $log, $location, 
         }
     }
     $scope.getQueries();
+
     //opens view modal
     $scope.view = function (q) {
         var modalInstance = $uibModal.open({
@@ -93,6 +109,7 @@ queries.controller('QueriesCtrl', function ($scope, $uibModal, $log, $location, 
             $scope.getQueries();
         })
     };
+
     //opens deleteQuery modal for query q
     $scope.deleteQuery = function (q) {
         var modalInstance = $uibModal.open({
@@ -128,31 +145,50 @@ queries.controller('QueriesCtrl', function ($scope, $uibModal, $log, $location, 
         });
     };
 });
+
 //controller for an instance of ViewQueryModal
 queries.controller('ViewQueryModalInstanceCtrl', function ($scope, $uibModalInstance, $log, query, queryService, issueService) {
+    //
     $scope.query = query;
+    //
     $scope.maxSteps = 2;
-    $scope.isSaveCollapsed = true;
+    //what step is the modal on
+    $scope.step = 1;
+    //
     $scope.tableResult = {};
+    //list of alerts
     $scope.alerts = [];
-    $scope.loadingPreview = false;
-    $scope.queryRanFine = true;
-    $scope.connectionFailed = false;
-    $scope.noResults = false;
+     //
     $scope.postReportSubmissionMessage = "";
+    //
+    $scope.isSaveCollapsed = true;
+    //
+    $scope.loadingPreview = false;
+    //
+    $scope.queryRanFine = true;
+    //
+    $scope.connectionFailed = false;
+    //
+    $scope.noResults = false;
+    //
     $scope.reportSubmitted = false;
+    //
+    $scope.shouldShowNotifyDevsForm = false;
+    //
+    $scope.shouldShowRequestForm = false;
+    //initialize new problem
     $scope.newProblemInput = {
         subject: '',
         body: ''
     }
+    //initialize a new request
     $scope.newRequestInput = {
         subject: '',
         body: '',
         type: 'VIEW'
     }
-    $scope.shouldShowNotifyDevsForm = false;
-    $scope.shouldShowRequestForm = false;
-    $scope.step = 1; //what step is the modal on
+
+
     //advance the modal to the next step
     $scope.next = function () {
         $scope.step++;
@@ -161,6 +197,7 @@ queries.controller('ViewQueryModalInstanceCtrl', function ($scope, $uibModalInst
             $scope.runQuery();
         }
     };
+
     //go back a step in the modal
     $scope.previous = function () {
         $scope.step--;
@@ -171,10 +208,13 @@ queries.controller('ViewQueryModalInstanceCtrl', function ($scope, $uibModalInst
         $scope.queryRanFine = true;
         $scope.noResults = false;
     };
+
     //dismiss the modal
     $scope.cancel = function () {
         $uibModalInstance.dismiss('cancel');
     };
+
+    //updates a query
     $scope.updateQuery = function () {
         queryService.updateQuery($scope.query, $scope.query.sqlString).then(function (response) {
             if (response.status == 'Success') {
@@ -194,6 +234,7 @@ queries.controller('ViewQueryModalInstanceCtrl', function ($scope, $uibModalInst
             });
         })
     };
+
     //save the edit query as a new query
     $scope.saveAs = function () {
         if ($scope.newQuery.description == null || $scope.newQuery.description == undefined) {
@@ -218,6 +259,7 @@ queries.controller('ViewQueryModalInstanceCtrl', function ($scope, $uibModalInst
             $log.debug($scope.statement);
         });
     };
+
     //create new zeppelin note
     $scope.exportZeppelin = function () {
         var newNote = {
@@ -233,6 +275,7 @@ queries.controller('ViewQueryModalInstanceCtrl', function ($scope, $uibModalInst
             window.open(window.zeppelin + '#/notebook/' + response.body);
         })
     };
+
     //run the query
     $scope.runQuery = function () {
         $scope.loadingPreview = true;
@@ -259,12 +302,18 @@ queries.controller('ViewQueryModalInstanceCtrl', function ($scope, $uibModalInst
             $scope.errorMsg = error.message;
         });
     };
+
+    //show notify devs form
     $scope.showNotifyDevsForm = function () {
         $scope.shouldShowNotifyDevsForm = true;
     };
+
+    //hide Notify Devs Form
     $scope.hideNotifyDevsForm = function () {
         $scope.shouldShowNotifyDevsForm = false;
     };
+
+    //creates and sends a new Discussion Thread - problem
     $scope.reportProblem = function () {
         issueService.createIssue($scope.newProblemInput).then(function (response) {
             // success
@@ -276,14 +325,20 @@ queries.controller('ViewQueryModalInstanceCtrl', function ($scope, $uibModalInst
             $scope.reportSubmitted = false;
         });
     };
+
+    //showRequestForm
     $scope.showRequestForm = function () {
         $scope.shouldShowRequestForm = true;
         $scope.newRequestInput.subject = 'Requesting new Table/View using the query ' + $scope.query.name;
         $scope.newRequestInput.body = $scope.query.sqlString;
     };
+
+    //hideRequestForm
     $scope.hideRequestForm = function () {
         $scope.shouldShowRequestForm = false;
     };
+
+    //creates and sends a new Discussion Thread - request statement
     $scope.sendRequest = function () {
         $scope.newRequestInput.subject = $scope.newRequestInput.type + " " + $scope.newRequestInput.subject;
         issueService.createIssue($scope.newRequestInput).then(function (response) {
@@ -297,6 +352,7 @@ queries.controller('ViewQueryModalInstanceCtrl', function ($scope, $uibModalInst
         });
     };
 });
+
 //controller for instance of QueryDeleteModal
 queries.controller('QueryDeleteModalCtrl', function ($scope, $uibModalInstance, $log, query) {
     $scope.query = query;
